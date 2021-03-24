0c006edde5cb8c2d005fd8b4f64cc4ad462328b3
Kamijouko <MikuVick7139@gmail.com>
2021-03-24 17:52:41 +0900

    1.0.0.2

--- AppLibrary/MainLoad.cs
+++ AppLibrary/MainLoad.cs
@@ -11,10 +11,12 @@ namespace AppLibrary
     {
         public List<Source> sourceList = new List<Source>(); 
 
-        private List<string> constSources = new List<string> { "Twitter@gamedukedeatho1.0", "Twitter@gamedukedeatho2.0", "NGA"/*, "Twitter@Lig_Rig"*/};
+        private List<string> constSources = new List<string> { "1Twitter@gamedukedeatho1.0", "2Twitter@gamedukedeatho2.0", "NGA"/*, "Twitter@Lig_Rig"*/};
 
-        public List<string> sources = new List<string> { "Twitter@gamedukedeatho1.0", "Twitter@gamedukedeatho2.0", "NGA"/*, "Twitter@Lig_Rig"*/ };
+        //添加新项只需将新数据的名称加入此列表即可，请勿加在开头。
+        public List<string> sources = new List<string> { "1Twitter@gamedukedeatho1.0", "2Twitter@gamedukedeatho2.0", "NGA"/*, "Twitter@Lig_Rig"*/ };
 
+        //在上表添加完名称后将静态数据原加进下表即可。名称需与上表新加入的一致。之后数据将自动读取。
         public List<Source> newSources = new List<Source>();
 
         public MainLoad()
